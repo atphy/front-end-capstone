a5c3451cb4abb17081e7e26a44bc85297c82de96
Austin Phy <atphy42@gmail.com>
2020-09-15 15:23:59 -0500

    auth functions complete

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,53 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 import './App.scss';
 
+import fbConnection from '../helpers/data/connection';
+
 import Auth from '../components/Auth/Auth';
+import Nav from '../components/Nav/Nav';
+import Profile from '../components/Profile/Profile';
+
+fbConnection();
 
 class App extends React.Component {
-  render() {
-    return (
-      <Auth />
-    );
-  }
+state = {
+  authed: false,
+}
+
+componentDidMount() {
+  this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      this.setState({ authed: true });
+    } else {
+      this.setState({ authed: false });
+    }
+  });
+}
+
+componentWillUnmount() {
+  this.removeListener();
+}
+
+render() {
+  const { authed } = this.state;
+
+  const loadComponent = () => {
+    if (authed) {
+      return <div>
+              <Nav />
+              <Profile />;
+            </div>;
+    }
+    return <Auth />;
+  };
+  return (
+    <div className="App">{loadComponent()}</div>
+
+  );
+}
 }
 
 export default App;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 import './Nav.scss';
 
 class Nav extends React.Component {
-  render() {
-    return (
+logoutClick = (e) => {
+  firebase.auth().signOut();
+}
+
+render() {
+  return (
       <nav>
-        <h1>Nav</h1>
+        <h1 className="nav-title">Timmi</h1>
+        <div className="anchor-holder">
+          <a className="nav-link">Get your private link for sharing</a>
+          <a className="nav-logout" onClick={this.logoutClick}>Log out</a>
+        </div>
       </nav>
-    );
-  }
+  );
+}
 }
 
 export default Nav;
